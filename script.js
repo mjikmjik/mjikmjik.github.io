215e83bd821f289227985ba64dd4eb2a4e632fc5
mjikmjik <juno020202@gmail.com>
2025-04-15 14:19:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeMessage() {
   const messages = [
-    "今日も頑張ろう！",
-    "プログラミングは楽しいね！",
-    "GitHub Pages、最高！",
-    "HTMLとCSSをマスターしよう！"
+    "螺旋階段に飲み込まれろ",
+    "お前の不幸が生きる喜び。",
+    "孤独死しろ",
+    "本当は好きだよ"
   ];
   const randomIndex = Math.floor(Math.random() * messages.length);
   document.getElementById("message").textContent = messages[randomIndex];
